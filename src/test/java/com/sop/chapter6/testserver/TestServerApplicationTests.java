d8297562c31e9b4c497da3fffe2b2bf053defb1d
Chanisaranest <63070031@it.kmitl.ac.th>
2022-10-04 11:46:01 +0700

    Initial commit

---
+++ src/test/java/com/sop/chapter6/testserver/TestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sop.chapter6.testserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
